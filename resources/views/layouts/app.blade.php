acbe94b6af328448625cdfbe7760ec99734a925d
Jose Lopez <jlopez@three29.com>
2017-10-24 10:29:21 -0700

    Add channel option included in create threads form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,10 @@
                     <ul class="nav navbar-nav">
                         &nbsp;<li><a href="/threads">All Threads</a></li>
 
+                        <li>
+                            <a href="/threads/create">New Thread</a>
+                        </li>
+
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                                aria-expanded="false">Channels <span class="caret"></span></a>
--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -14,16 +14,42 @@
                             {{ csrf_field() }}
 
                             <div class="form-group">
+                                <label for="channel_id">Choose a Channel:</label>
+                                <select type="text" class="form-control" id="channel_id" name="channel_id" required>
+                                    <option value="">Choose One...</option>
+                                    @foreach( App\Channel::all() as $channel )
+                                        <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
+                                            {{ $channel->name }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group">
                                 <label for="title">Title</label>
-                                <input type="text" class="form-control" id="title" name="title">
+                                <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}" required>
                             </div>
 
                             <div class="form-group">
                                 <label for="body">Body</label>
-                                <textarea type="text" class="form-control" id="body" rows="8" name="body"></textarea>
+                                <textarea type="text" class="form-control" id="body" rows="8" name="body" required>{{ old('body') }}</textarea>
                             </div>
 
-                            <button type="submit" class="btn btn-primary">Publish</button>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Publish</button>
+                            </div>
+
+                            @if( count( $errors ) )
+
+                                @foreach( $errors->all() as $error )
+
+                                    <ul class="alert alert-danger">
+                                        <li>{{ $error  }}</li>
+                                    </ul>
+
+                                @endforeach
+
+                            @endif
 
                         </form>
 
