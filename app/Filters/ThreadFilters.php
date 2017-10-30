4512cf9dde34b704e37b594aa261ea54f6f72362
jlopez1423 <jlopez46@ucsc.edu>
2017-10-29 17:47:30 -0700

    Code refactor

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -7,30 +7,15 @@ namespace App\Filters;
 use App\User;
 use Illuminate\Http\Request;
 
-class ThreadFilters
+class ThreadFilters extends Filters
 {
-    protected $builder;
+    //All of the filters we can respond to
+    protected $filters = ['by'];
 
     /**
-     * ThreadFilters constructor.
-     */
-    public function __construct( Request $request )
-    {
-        $this->request = $request;
-    }
-
-    public function apply( $builder )
-    {
-        $this->builder = $builder;
-        if( ! $username = $this->request->by ) return $builder;
-
-        //We apply our filters to the builder
-        return $this->by( $username );
-
-    }
-
-    /**
-     * @param $username
+     * Filter a query by a given username
+     *
+     * @param string $username
      * @return mixed
      */
     protected function by( $username )
